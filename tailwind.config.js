e60febe28398b1282fba6214afc950eac976ec48
imane bouboul <90159508+HourSecret@users.noreply.github.com>
2024-04-14 10:04:27 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+
+import withMT from "@material-tailwind/react/utils/withMT"
+ 
+export default withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  
+  ],
+  theme: {
+    
+      extend: {
+        colors: {
+          main_color: '#fe300b',
+          white_color: '#f4f8ff',
+        },
+      },
+    
+   
+  },
+  plugins: [],
+});
